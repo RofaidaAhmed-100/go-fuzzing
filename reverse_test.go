ba6b89dd4fe2c917d250440ef4574754ce2d0c8e
Rofaida Ahmed <rofaidaahmed879@gmail.com>
2025-11-27 19:42:43 +0200

    Fix the invalid string error

--- reverse_test.go
+++ reverse_test.go
@@ -10,13 +10,14 @@ func FuzzReverse(f *testing.F) {
         f.Add(tc)  // Use f.Add to provide a seed corpus
     }
     f.Fuzz(func(t *testing.T, orig string) {
-        rev := Reverse(orig)
-        doubleRev := Reverse(rev)
-        if orig != doubleRev {
-            t.Errorf("Before: %q, after: %q", orig, doubleRev)
-        }
-        if utf8.ValidString(orig) && !utf8.ValidString(rev) {
-            t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
-        }
-    })
+    rev := Reverse(orig)
+    doubleRev := Reverse(rev)
+    t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+    if orig != doubleRev {
+        t.Errorf("Before: %q, after: %q", orig, doubleRev)
+    }
+    if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+        t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+    }
+})
 }
